ce480923e94625a07d2a480a7d54d928a57f6a2b
Indranil Halder <indranilhalder.dev@gmail.com>
2023-07-16 01:03:20 +0530

    Initial Project Setup

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "../styles/global.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Promptopia",
+  description: "AI Prompt generation social media.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
